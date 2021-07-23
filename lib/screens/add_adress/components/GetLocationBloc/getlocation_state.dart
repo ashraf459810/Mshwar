b8163c60b357467a20558545036e9426676f1e92
ashraf459810 <ashraf459810@gmail.com>
2021-07-24 02:10:22 +0300

    add address done with api

---
+++ lib/screens/add_adress/components/GetLocationBloc/getlocation_state.dart
@@ -0,0 +1,27 @@
+part of 'getlocation_bloc.dart';
+
+@immutable
+abstract class GetlocationState {}
+
+class GetlocationInitial extends GetlocationState {}
+
+class LocationState extends GetlocationState {
+  final Position position;
+  final String city;
+  final String country;
+  final String street;
+
+  LocationState(this.position, this.city, this.country, this.street);
+}
+
+class Error extends GetlocationState {
+  final String error;
+  Error(this.error);
+}
+
+class Loading extends GetlocationState {}
+
+class AddAddressState extends GetlocationState {
+  final AddAddressResponse addAddressResponse;
+  AddAddressState(this.addAddressResponse);
+}
